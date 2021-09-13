45e4e4948a6b92b86808e4e672da24e956f61a9e
halezmarie <teacherhaleymarie@gmail.com>
2021-09-13 14:08:58 -0500

    put fetch into a function, getMovies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 // first step: make a fetch request to  my baseURL and display items on the DOM 😁
 // second step: add adding description, rating, length, image, and where to watch it *** need to figure out how to get the image to show up and hyperlink for watch
+// third step: make it to where users can add a movie sucessfully! add an eventlistener, etc 😁
 
 const movieslist = document.getElementById('movies')
 const form = document.getElementById('movie-form')
@@ -11,7 +12,14 @@ const imageInput = document.getElementById('movie-image')
 const descriptionInput = document.getElementById('movie-description')
 const watchInput = document.getElementById('movie-watch')
 
+form.addEventListener('submit', handleSubmit)
 
+function handleSubmit(){
+
+
+}
+
+function getMovies(){
   fetch('http://localhost:3000/api/v1/movies')
 // always after a fetch request we must handle our response with a .then. We need to convert it so that it is readable and usable! That is why we parse the JSON. string value > data we can use
     .then(res => res.json())
@@ -19,11 +27,12 @@ const watchInput = document.getElementById('movie-watch')
     // magic
     .then(renderMovies)
     // ^ this does not need () because I want to call it later on. I am just listing the definition for now
+}
 
 
  // iterate over the array and for each movie I call a different function, when I finish filling out the form we will get data back that we can feed back into our renderMovies function
-  function renderMovies(rando){
-    const movies = rando["data"]
+function renderMovies(rando){
+  const movies = rando["data"]
     movies.forEach(element => {
         renderMovie(element)
     })
@@ -46,3 +55,4 @@ function renderMovie(movie){
     movieslist.appendChild(li)
 }
 
+getMovies()
