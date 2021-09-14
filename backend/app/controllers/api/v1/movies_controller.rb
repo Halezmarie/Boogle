cc292ea7b0730eee27b168113628c9fabdf87010
halezmarie <teacherhaleymarie@gmail.com>
2021-09-14 12:35:52 -0500

    Finally am able to create a movie, but I have to refresh still

--- backend/app/controllers/api/v1/movies_controller.rb
+++ backend/app/controllers/api/v1/movies_controller.rb
@@ -14,30 +14,34 @@ class Api::V1::MoviesController < ApplicationController
     end
 
     def create 
-        movie = Movie.create(movie_params)
+        movie = Movie.new(movie_params)
+        movie.category = Category.last # will change this when I work on my categories in the frontend
         # only if the movie saves then render json
         if movie.save
             render json: MovieSerializer.new(movie)
+        else
+            render json: {error: "👻 Oh no! You couldn't save #{movie.title}!"}
         end
     end
 
-    def destroy 
-        # finding by the id itself
-        movie = Movie.find_by_id(params[:id])
-        movie.destroy
-        render json: {message: "👻You successfully deleted #{movie.title}!"}
-    end
+    # def destroy 
+    #     # finding by the id itself
+    #     movie = Movie.find_by_id(params[:id])
+    #     movie.destroy
+    #     render json: {message: "👻You successfully deleted #{movie.title}!"}
+    # end
 
-    def update 
-        movie = Movie.find(params[:id])
-        if movie update(movie_params)
-            render json: MovieSerializer.new(movie)
-        end
-    end
+    # def update 
+    #     movie = Movie.find(params[:id])
+    #     if movie update(movie_params)
+    #         render json: MovieSerializer.new(movie)
+    #     end
+    # end
         
     private
-    
+
+    # keep category ids?
     def movie_params
-        params.require(:movie).permit(:title, :year, :rating, :length, :image, :description, :watch, :category_ids [])
+        params.require(:movie).permit(:title, :year, :rating, :length, :description, :watch, :image, :category_ids [])
     end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,8 +2,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :movies, only: [:index, :create, :destroy]
-      resources :categories, only: [:index]
+      resources :movies
+      # [:index, :create, :destroy]
+      resources :categories
+      #  only: [:index]
       # I do not want others to delete categories 
     end
   end
