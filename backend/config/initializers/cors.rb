fae4356895827bee221e0a64a984233e7f574108
Halezmarie <68069673+Halezmarie@users.noreply.github.com>
2021-10-06 01:00:55 -0500

    Commenting out what * means in cors.rb

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
+# currently accepting requests from ANY sources, it would be really insecure -- these settings are just for testing
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
